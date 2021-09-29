00e77a0127b35c4d4835cf41c168e725cce79301
iamnirmala <nshyamala1260@gmail.com>
2021-09-29 00:01:23 -0400

    Adding testing libraries and maven project.

---
+++ src/test/java/com/google/tests/FirstTest.java
@@ -0,0 +1,33 @@
+package com.google.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * This class has dummy test cases
+ *
+ * @author nirmala
+ */
+public class FirstTest {
+
+    /**
+     * This test case checks hello world text.
+     */
+
+    @Test
+    public void verifyHelloWorld() {
+        String actualText = "Hello World";
+        String expectedText = "Hello World";
+
+        Assert.assertEquals(actualText, expectedText, String.format("Expected is %s, but actual is %s", expectedText, actualText));
+    }
+
+    @Test
+    public void verifyAddition() {
+       int result = 10;
+        int a = 4;
+        int b = 3;
+        int actualResult = a + b;
+        Assert.assertEquals(actualResult, result, String.format("Expected is %s, but actual is %s", result, actualResult));
+    }
+}
